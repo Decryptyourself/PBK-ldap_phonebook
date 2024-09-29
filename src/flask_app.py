a8f2b1965b9b193f80fbe360c02a6d5b99c00d3e
KakDelishki <tyroneunknown@ya.ru>
2024-09-29 20:26:00 +0300

    29-09-2024 (add global depts)

--- src/flask_app.py
+++ src/flask_app.py
@@ -6,10 +6,60 @@ app = Flask(__name__)
 
 @app.route('/search')
 def search():
-    query = request.args.get('query', '')
-    query = query.lower() # Приведение запроса к нижнему регистру
-    departments = get_employees_by_department(query)
-    return jsonify(departments) # Возврат данных в JSON для клиента
+    query = request.args.get('query', '').lower()  # Приведение запроса к нижнему регистру
+    data = get_employees_by_global_department_with_query(query)
+    return jsonify(data)  # Возврат данных в JSON для клиента
+
+# Получение данных по департаментам и отделам
+def get_employees_by_global_department_with_query(query=None):
+    connection = psycopg2.connect(
+        host=DB_HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+    cursor = connection.cursor()
+
+    sql_query = """
+        SELECT global_department, department, ip_phone, ip_phone_second, name, position, email, mobile_phone
+        FROM users
+    """
+
+    params = []
+
+    if query:
+        sql_query += " WHERE LOWER(name) LIKE %s OR ip_phone LIKE %s"
+        params.extend([f'%{query}%', f'%{query}%'])
+
+    sql_query += " ORDER BY global_department, department;"
+
+    cursor.execute(sql_query, params)
+    employees = cursor.fetchall()
+
+    data = {}
+    for employee in employees:
+        global_department, department, ip_phone, ip_phone_second, name, position, email, mobile_phone = employee
+        if global_department not in data:
+            data[global_department] = {}
+        if department not in data[global_department]:
+            data[global_department][department] = []
+        data[global_department][department].append({
+            'ip_phone': ip_phone,
+            'ip_phone_second': ip_phone_second,
+            'name': name,
+            'position': position,
+            'email': email,
+            'mobile_phone': mobile_phone
+        })
+
+    # Применяем сортировку внутри каждого отдела
+    for global_department in data:
+        for department in data[global_department]:
+            data[global_department][department] = sort_employees(data[global_department][department])
+
+    cursor.close()
+    connection.close()
+    return data
 
 # Запрос из БД по департаментам
 def get_employees_by_global_department():
@@ -20,31 +70,22 @@ def get_employees_by_global_department():
         password=DB_PASSWORD
     )
     cursor = connection.cursor()
+
     cursor.execute("""
-        SELECT global_department
+        SELECT DISTINCT global_department
         FROM users
-        GROUP BY global_department
         ORDER BY global_department;
     """)
-    global_departments = cursor.fetchall()
+    global_depts = cursor.fetchall()
 
-    global_departments = {}
-    for gl_department in global_departments:
-        global_department = gl_department
-        if gl_department not in global_departments:
-            global_departments[gl_department] = []
-        global_departments[gl_department].append({
-            'global_department': global_department
-        })
+    global_departments = [global_dept_tuple[0] for global_dept_tuple in global_depts]
 
     cursor.close()
     connection.close()
-    
-    print(global_departments)
     return global_departments
 
-# Запрос из БД по отделам
-def get_employees_by_department(query=None):
+# Получение сотрудников по отделам с возможностью фильтрации по департаменту
+def get_employees_by_department(query=None, global_department=None):
     connection = psycopg2.connect(
         host=DB_HOST,
         dbname=DB_NAME,
@@ -53,20 +94,28 @@ def get_employees_by_department(query=None):
     )
     cursor = connection.cursor()
 
-    # Если запрос передан --> поиск по имени или внутреннему номеру телефона
+    sql_query = """
+        SELECT ip_phone, ip_phone_second, name, department, position, email, mobile_phone
+        FROM users
+    """
+
+    params = []
+    conditions = []
+
+    if global_department:
+        conditions.append("global_department = %s")
+        params.append(global_department)
+
     if query:
-        cursor.execute("""
-            SELECT ip_phone, ip_phone_second, name, department, position, email, mobile_phone
-            FROM users
-            WHERE LOWER(name) LIKE %s OR ip_phone LIKE %s
-            ORDER BY department, ip_phone;
-        """, (f'%{query}%', f'%{query}%'))
-    else:
-        cursor.execute("""
-            SELECT ip_phone, ip_phone_second, name, department, position, email, mobile_phone
-            FROM users
-            ORDER BY department, ip_phone;
-        """)
+        conditions.append("(LOWER(name) LIKE %s OR ip_phone LIKE %s)")
+        params.extend([f'%{query}%', f'%{query}%'])
+
+    if conditions:
+        sql_query += " WHERE " + " AND ".join(conditions)
+
+    sql_query += " ORDER BY department;"
+
+    cursor.execute(sql_query, params)
     employees = cursor.fetchall()
 
     departments = {}
@@ -82,15 +131,50 @@ def get_employees_by_department(query=None):
             'email': email,
             'mobile_phone': mobile_phone
         })
+
+    # Сортировка внутри каждого отдела
+    for department in departments:
+        departments[department] = sort_employees(departments[department])
+
     cursor.close()
-    connection.close()    
+    connection.close()
     return departments
 
-# Путь для отображения главной страницы
+def sort_employees(employees):
+    # Сортировка сотрудников внутри отдела
+    def sort_key(employee):
+        position = employee['position'].lower()
+        if 'руководитель' in position and 'заместитель' not in position:
+            return (0, 0)  # Руководитель - первый
+        elif 'заместитель руководителя' in position:
+            return (1, 0)  # Заместитель руководителя - второй
+        else:
+            # Преобразование ip_phone в число для корректной сортировки
+            try:
+                ip_phone = int(employee['ip_phone'])
+            except ValueError:
+                ip_phone = float('inf')  # Если не число, то в конец
+            return (2, ip_phone)  # Остальные сортируются по внутреннему номеру
+    # Сортировка списка сотрудников по ключу
+    return sorted(employees, key=sort_key)
+
+@app.route('/get_global_departments')
+def get_global_departments():
+    global_departments = get_employees_by_global_department()
+    return jsonify(global_departments)
+
+@app.route('/get_departments')
+def get_departments():
+    global_department = request.args.get('global_department', '')
+    departments = get_employees_by_department(global_department=global_department)
+    return jsonify(departments)
+
 @app.route('/')
 def index():
     global_departments = get_employees_by_global_department()
     departments = get_employees_by_department()
+    print(global_departments)
+    print(departments)
     return render_template('index.html', departments=departments, global_departments=global_departments)
 
 # ДЛЯ ТЕСТОВ, БЕЗ WSGI
--- src/static/script.js
+++ src/static/script.js
@@ -1,24 +1,125 @@
-// Главная функция для отображения шапок отделов при пустом поисковом запросе
-function showStaticHeaders(departments, global_departments) {
+// Функция инициализации обработчиков событий на заголовки департаментов
+function initializeGlobalDepartmentHeaders() {
+    document.querySelectorAll('.global-department-header').forEach(function (deptHeader) {
+        deptHeader.addEventListener('click', function () {
+            var searchField = document.getElementById('search').value.trim();
+            if (searchField === '') {  // Проверяем, что поле поиска пустое
+                var globalDepartment = this.textContent.trim();
+
+                fetch('/get_departments?global_department=' + encodeURIComponent(globalDepartment))
+                    .then(response => response.json())
+                    .then(departments => {
+                        showDepartments(departments);
+                    });
+            }
+        });
+    });
+}
+
+// Функция инициализации обработчиков событий на заголовки отделов
+function initializeDepartmentHeaders() {
+    document.querySelectorAll('.department-header').forEach(function (departmentHeader) {
+        departmentHeader.addEventListener('click', function () {
+            var siblingTable = this.nextElementSibling;
+
+            if (siblingTable.style.display === 'none' || siblingTable.style.display === '') {
+                siblingTable.style.display = 'table';
+            } else {
+                siblingTable.style.display = 'none';
+            }
+        });
+    });
+}
+
+function loadGlobalDepartments() {
+    fetch('/get_global_departments')
+        .then(response => response.json())
+        .then(global_departments => {
+            var globalStaticTablesDiv = document.getElementById('global-static-tables');
+            globalStaticTablesDiv.innerHTML = '';
+
+            global_departments.forEach(function(global_department, index) {
+                var global_departmentTable = document.createElement('div');
+                global_departmentTable.classList.add('global-department-table');
+
+                var global_departmentHeader = document.createElement('h2');
+                global_departmentHeader.textContent = global_department;
+                global_departmentHeader.classList.add('global-department-header');
+                global_departmentHeader.id = 'global-department-header-' + (index + 1);
+                global_departmentTable.appendChild(global_departmentHeader);
+
+                globalStaticTablesDiv.appendChild(global_departmentTable);
+            });
+
+            enableGlobalDepartmentClicks();  // Активация кликов по департаментам
+        });
+}
+
+function showDepartments(departments) {
     var staticTablesDiv = document.getElementById('static-tables');
-    staticTablesDiv.innerHTML = '';  // Очистка содержимого
+    staticTablesDiv.innerHTML = '';  // Очистка правого блока
 
-    // Прохождение по департаментам и создание шапки для каждого департамента
-    for (var global_department in global_departments) {
-        var departmentHeader = document.createElement('h2');
-        departmentHeader.textContent = global_department;
-        departmentHeader.classList.add('department-header');
-        staticTablesDiv.appendChild(departmentHeader);
-    }
-
-    // Прохождение по отделам и создание шапки для каждого отдела
     for (var department in departments) {
-        var departmentHeader = document.createElement('h2');
+        var departmentHeader = document.createElement('h3');
         departmentHeader.textContent = department;
         departmentHeader.classList.add('department-header');
         staticTablesDiv.appendChild(departmentHeader);
 
-        // Создание таблицы с сотрудниками, но она скрыта по умолчанию
+        var table = document.createElement('table');
+        table.classList.add('department-table');
+        table.style.display = 'none';  // Таблицы скрыты по умолчанию
+
+        var tbody = document.createElement('tbody');
+
+        departments[department].forEach(function (user) {
+            var row = document.createElement('tr');
+            row.innerHTML = '<td>' + user.ip_phone + '</td>' +
+                            '<td>' + user.name + '</td>' +
+                            '<td>' + user.position + '</td>' +
+                            '<td><a href="mailto:' + user.email + '">' + user.email + '</a></td>' +
+                            '<td>' + user.mobile_phone + '</td>';
+            tbody.appendChild(row);
+        });
+
+        table.appendChild(tbody);
+        staticTablesDiv.appendChild(table);
+    }
+
+    initializeDepartmentHeaders(); // Инициализация обработчика на заголовки отделов
+}
+
+// Функция отображения статичных заголовков отделов при первой загрузке страницы
+function showStaticHeaders(departments, global_departments) {
+    var staticTablesDiv = document.getElementById('static-tables');
+    var globalStaticTablesDiv = document.getElementById('global-static-tables');
+    staticTablesDiv.innerHTML = '';  // Очистка содержимого
+    globalStaticTablesDiv.innerHTML = '';  // Очистка содержимого для глобальных департаментов
+
+    // Прохождение по глобальным департаментам и создание шапки для каждого
+    global_departments.forEach(function(global_department, index) {
+        // Создание контейнера для каждого департамента
+        var global_departmentTable = document.createElement('div');
+        global_departmentTable.classList.add('global-department-table');
+        
+        // Создание заголовка для глобального департамента
+        var global_departmentHeader = document.createElement('h2');
+        global_departmentHeader.textContent = global_department;
+        global_departmentHeader.classList.add('global-department-header');
+        global_departmentHeader.id = 'global-department-header-' + (index + 1);  // Уникальный ID
+        global_departmentTable.appendChild(global_departmentHeader);
+        
+        // Добавление заголовка глобального департамента в контейнер для глобальных департаментов
+        globalStaticTablesDiv.appendChild(global_departmentTable);
+    });
+
+    // Прохождение по отделам и создание шапки для каждого отдела
+    for (var department in departments) {
+        var departmentHeader = document.createElement('h3');
+        departmentHeader.textContent = department;
+        departmentHeader.classList.add('department-header');
+        staticTablesDiv.appendChild(departmentHeader);
+
+        // Создание таблицы с сотрудниками (скрыта по умолчанию)
         var table = document.createElement('table');
         table.classList.add('department-table');
         table.style.display = 'none';
@@ -34,156 +135,85 @@ function showStaticHeaders(departments, global_departments) {
 
         table.appendChild(tbody);
         staticTablesDiv.appendChild(table);
-
-        // Обработчик нажатия для показа/скрытия сотрудников
-        departmentHeader.addEventListener('click', function () {
-            var siblingTable = this.nextElementSibling;  // Таблица, следующая за заголовком
-
-            // Проверка текущего состояние таблицы (показана или скрыта)
-            if (siblingTable.style.display === 'none') {
-                setTimeout(() => { siblingTable.style.display = 'table'; }, 3000); // Попытка сделать паузу (неудачно)
-                //siblingTable.style.display = 'table';  // Показываем таблицу только для этого отдела
-            } else {
-                siblingTable.style.display = 'none';  // Скрытие таблицы
-            }
-        });
     }
+
+    // Инициализация обработчиков событий на заголовки отделов
+    initializeDepartmentHeaders();
 }
 
-// Функция для обработки поиска
-function handleSearch(query, data) {
-    var resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = '';  // Очистка результатов поиска
+function disableGlobalDepartmentClicks() {
+    document.querySelectorAll('.global-department-header').forEach(function (deptHeader) {
+        var newDeptHeader = deptHeader.cloneNode(true);
+        deptHeader.parentNode.replaceChild(newDeptHeader, deptHeader);
+    });
+}
 
-    var isNumeric = /^\d+$/.test(query);  // Проверка на цифры
+function enableGlobalDepartmentClicks() {
+    initializeGlobalDepartmentHeaders();
+}
+
+// Функция обработки поиска
+function handleSearch(query, data) {
+    var staticTablesDiv = document.getElementById('static-tables');
+    staticTablesDiv.innerHTML = '';  // Очистка правого блока
 
     if (query && Object.keys(data).length > 0) {
-        // Прохождение по отделам и создание таблицы для каждого отдела
-        for (var department in data) {
-            var employeesFound = false;
-            var departmentHeader = document.createElement('h2');
-            var table = document.createElement('table');
-            var tbody = document.createElement('tbody');
+        // Прохождение по всем департаментам
+        for (var globalDepartment in data) {
+            var departments = data[globalDepartment];
 
-            data[department].forEach(function (user) {
-                var row = document.createElement('tr');
-                
-                // Поиск по номеру телефона (только цифры)
-                if (isNumeric) {
-                    var truncatedPhone = user.ip_phone.substring(0, query.length);
-                    if (truncatedPhone === query) {
-                        row.innerHTML = '<td class="column-ip-phone">' + user.ip_phone + '</td>' +
-                                        '<td class="column-name">' + user.name + '</td>' +
-                                        '<td class="column-position">' + user.position + '</td>' +
-                                        '<td class="column-email"><a href="mailto:' + user.email + '">' + user.email + '</a></td>' +
-                                        '<td class="column-mobile-phone">' + user.mobile_phone + '</td>';
-                        tbody.appendChild(row);
-                        employeesFound = true;
-                    }
-                // Поиск по имени (без учета регистра)
-                } else {
-                    var lowerCaseQuery = query.toLowerCase();
-                    var lowerCaseName = user.name.toLowerCase();
-                    if (lowerCaseName.includes(lowerCaseQuery)) {
-                        row.innerHTML = '<td class="column-ip-phone">' + user.ip_phone + '</td>' +
-                                        '<td class="column-name">' + user.name + '</td>' +
-                                        '<td class="column-position">' + user.position + '</td>' +
-                                        '<td class="column-email"><a href="mailto:' + user.email + '">' + user.email + '</a></td>' +
-                                        '<td class="column-mobile-phone">' + user.mobile_phone + '</td>';
-                        tbody.appendChild(row);
-                        employeesFound = true;
-                    }
-                }
-            });
-
-            if (employeesFound) {
+            // Прохождение по отделам внутри департамента
+            for (var department in departments) {
+                var departmentHeader = document.createElement('h3');
                 departmentHeader.textContent = department;
+                departmentHeader.classList.add('department-header');
+                staticTablesDiv.appendChild(departmentHeader);
+
+                // Создание таблицы с сотрудниками (раскрыта по умолчанию)
+                var table = document.createElement('table');
+                table.classList.add('department-table');
+                table.style.display = 'table';  // Показ таблицы
+
+                var tbody = document.createElement('tbody');
+
+                departments[department].forEach(function (user) {
+                    var row = document.createElement('tr');
+                    row.innerHTML = '<td>' + user.ip_phone + '</td>' +
+                                    '<td>' + user.name + '</td>' +
+                                    '<td>' + user.position + '</td>' +
+                                    '<td><a href="mailto:' + user.email + '">' + user.email + '</a></td>' +
+                                    '<td>' + user.mobile_phone + '</td>';
+                    tbody.appendChild(row);
+                });
+
                 table.appendChild(tbody);
-                resultsDiv.appendChild(departmentHeader);
-                resultsDiv.appendChild(table);
+                staticTablesDiv.appendChild(table);
             }
         }
-
-        if (!resultsDiv.querySelector('table')) {
-            var noResults = document.createElement('p');
-            noResults.textContent = 'Не найдено';
-            resultsDiv.appendChild(noResults);
-        }
     } else {
-        var noResults = document.createElement('p');
-        noResults.textContent = 'Не найдено';
-        resultsDiv.appendChild(noResults);
+        staticTablesDiv.innerHTML = '<p class="not-found">Не найдено</p>';
     }
 }
 
 // Основная функция обработки ввода в поле поиска
 document.getElementById('search').addEventListener('input', function () {
     var query = this.value.trim();
-
-    fetch('/search?query=' + encodeURIComponent(query))  // Кодировка запроса
+    fetch('/search?query=' + encodeURIComponent(query))
         .then(response => response.json())
         .then(data => {
-            var staticTablesDiv = document.getElementById('static-tables');
-            var resultsDiv = document.getElementById('results');
-
             if (query) {
-                resultsDiv.classList.remove('hidden');
-                staticTablesDiv.classList.add('hidden');  // Скрываем статичные таблицы при поиске
-                handleSearch(query, data);  // Выполняем поиск
+                handleSearch(query, data);  // Отображение результатов поиска
+                disableGlobalDepartmentClicks();  // Деактивация кликов по департаментам
             } else {
-                resultsDiv.classList.add('hidden');
-                staticTablesDiv.classList.remove('hidden');  // Показываем статичные таблицы при очистке поиска
-                showStaticHeaders(data);  // Если поиска нет, показываем шапки отделов
+                loadGlobalDepartments();  // Загрузка все департаменты
+                enableGlobalDepartmentClicks();  // Активация кликов по департаментам
+                document.getElementById('static-tables').innerHTML = '';  // Очистка правого блока
             }
         });
-});// Основная функция обработки ввода в поле поиска
-
-// Обработчик событий на все заголовки отделов
-document.querySelectorAll('.department-header').forEach(function (departmentHeader) {
-    departmentHeader.addEventListener('click', function () {
-        var siblingTable = this.nextElementSibling;  // Таблица, следующая за заголовком
-
-        // Проверка текущего состояние таблицы (показана или скрыта)
-        if (siblingTable.style.display === 'none' || siblingTable.style.display === '') {
-            siblingTable.style.display = 'table';  // Показываем таблицу только для этого отдела
-        } else {
-            siblingTable.style.display = 'none';  // Скрываем таблицу
-        }
-    });
 });
 
-// Функция для отображения статичных заголовков отделов при первой загрузке страницы
-function showStaticHeaders(data) {
-    var staticTablesDiv = document.getElementById('static-tables');
-    var resultsDiv = document.getElementById('results');
-
-    resultsDiv.classList.add('hidden');  // Скрытие результатов поиска
-    staticTablesDiv.classList.remove('hidden');  // Показ статичных таблиц
-
-    // Отображение заголовков отделов
-    data.forEach(function(department) {
-        var header = document.createElement('h2');
-        header.className = 'department-header';
-        header.textContent = department.name;  //имя отдела
-        staticTablesDiv.appendChild(header);
-
-        // Таблица для отдела (по умолчанию скрыта)
-        var table = document.createElement('table');
-        table.style.display = 'none';  // Скрыта по умолчанию
-        staticTablesDiv.appendChild(table);
-    });
-
-// Итог: после создания всех заголовков добавление к ним обработчика кликов (fix, иначе не работает)
-document.querySelectorAll('.department-header').forEach(function (departmentHeader) {
-    departmentHeader.addEventListener('click', function () {
-        var siblingTable = this.nextElementSibling;  // Таблица, следующая за заголовком
-        // Проверка текущего состояния таблицы (показана или скрыта)
-        if (siblingTable.style.display === 'none' || siblingTable.style.display === '') {
-            setTimeout(() => { siblingTable.style.display = 'table'; }, 3000); // Попытка сделать паузу (неудачно)
-            //siblingTable.style.display = 'table';  // Показываем таблицу только для этого отдела
-        } else {
-            siblingTable.style.display = 'none';  // Скрытие таблицы
-        }
-    });
-});
-}
+// Вызов функции для отображения статичных заголовков при загрузке страницы
+window.onload = function () {
+    loadGlobalDepartments();  // Загрузка департаментов в левом блоке div2
+    document.getElementById('static-tables').innerHTML = '';  // Очистка правого блока div4
+};
